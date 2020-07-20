b4221d75c1192173a197f39ca34540af8b55fa76
Set <6942774+setzerbomb@users.noreply.github.com>
2020-07-20 17:06:06 -0300

    .

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Routes from './routes';
 import { ToastContainer } from 'react-toastify';
 
 import './config/ReactotronConfig';
 
+import Routes from './routes';
 import GlobalStyle from './styles/global';
 
 import store from './store/';
